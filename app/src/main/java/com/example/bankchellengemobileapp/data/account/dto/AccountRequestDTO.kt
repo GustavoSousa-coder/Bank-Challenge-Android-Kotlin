663bc93178aefc42e2c940a62861534901c661fa
Luis Gustavo <sousa.gustavo.dev@gmail.com>
2026-07-22 20:37:07 -0300

    feat: novas funcionalidades e iniciação de alinhamento com DTOs da API

--- app/src/main/java/com/example/bankchellengemobileapp/data/account/dto/AccountRequestDTO.kt
+++
@@ -1,7 +0,0 @@
-package com.example.bankchellengemobileapp.data.account.dto
-
-import com.example.bankchellengemobileapp.data.enums.AccountType
-
-data class AccountRequestDTO(
-    val type: AccountType
-)
---
+++ app/src/main/java/com/example/bankchellengemobileapp/ui/transactions/TransactionsActivity.kt
@@ -0,0 +1,4 @@
+package com.example.bankchellengemobileapp.ui.transactions
+
+class TransactionsActivity {
+}
