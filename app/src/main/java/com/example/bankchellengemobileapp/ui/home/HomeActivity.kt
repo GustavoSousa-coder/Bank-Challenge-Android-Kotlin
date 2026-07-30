3a83250b0228fa69cc4ba1541bdef5a2ce17a11c
Luis Gustavo <sousa.gustavo.dev@gmail.com>
2026-07-30 17:09:32 -0300

    fix: estado quebrado apos redesign das telas o projeto seguirá com a versão estável

--- app/src/main/java/com/example/bankchellengemobileapp/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/bankchellengemobileapp/ui/home/HomeActivity.kt
@@ -3,12 +3,15 @@ package com.example.bankchellengemobileapp.ui.home
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.bankchellengemobileapp.R
 import com.example.bankchellengemobileapp.ui.card.VirtualCardManagementActivity
 import com.example.bankchellengemobileapp.ui.pix.PaymentPixActivity
 import com.example.bankchellengemobileapp.ui.pix.PixKeyManagementActivity
-import com.example.bankchellengemobileapp.ui.pix.RegisterPixKeyActivity
+import com.example.bankchellengemobileapp.ui.statistics.StatisticsActivity
 import com.example.bankchellengemobileapp.ui.transactions.TransactionsActivity
 
 class HomeActivity : AppCompatActivity() {
@@ -16,27 +19,36 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val btnTransfer: Button = findViewById(R.id.btnTransferir)
-        btnTransfer.setOnClickListener {
+        val btnTransferir: Button = findViewById(R.id.btnTransferir)
+        btnTransferir.setOnClickListener {
             startActivity(Intent(this, TransactionsActivity::class.java))
         }
 
-        val btnPix: Button = findViewById(R.id.btnPix)
-        btnPix.setOnClickListener {
+        val btnMinhasChaves: Button = findViewById(R.id.btnMinhasChaves)
+        btnMinhasChaves.setOnClickListener {
             startActivity(Intent(this, PixKeyManagementActivity::class.java))
         }
 
-        val btnAddressKey: Button = findViewById(R.id.btnAddressKey)
-        btnAddressKey.setOnClickListener {
-            startActivity(Intent(this, RegisterPixKeyActivity::class.java))
+        val btnPagarComPix: Button = findViewById(R.id.btnPagarComPix)
+        btnPagarComPix.setOnClickListener {
+            startActivity(Intent(this, PaymentPixActivity::class.java))
         }
 
-        val btnCardVirtual: Button = findViewById(R.id.btnCardVirtual)
-        btnCardVirtual.setOnClickListener {
+        val btnMinhasEstatisticas: Button = findViewById(R.id.btnMinhasEstatisticas)
+        btnMinhasEstatisticas.setOnClickListener {
+            startActivity(Intent(this, StatisticsActivity::class.java))
+        }
 
-
-
+        val btnMeusCartoes: Button = findViewById(R.id.btnMeusCartoes)
+        btnMeusCartoes.setOnClickListener {
             startActivity(Intent(this, VirtualCardManagementActivity::class.java))
+        }
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
+        val btnMenu: ImageButton = findViewById(R.id.btnMenu)
+
+        btnMenu.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
         }
 
     }
